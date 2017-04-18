a3a75484cb6d8b1eeb6921c3583fe797a0b02c3c
Anurag Solanki <anurag.solanki.mail@gmail.com>
2017-04-17 21:26:13 -0700

    Merged with sidd_location_branch code

---
+++ app/src/main/java/com/asu/mc/digitalassist/activities/rsclient/GetLocationFromLatLong.java
@@ -0,0 +1,29 @@
+package com.asu.mc.digitalassist.activities.rsclient;
+
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by anurag on 4/17/17.
+ */
+public class GetLocationFromLatLong {
+
+    String latitude="";
+    String longitude="";
+    Geocoder geocoder;
+    List<Address> addresses;
+
+    geocoder = new Geocoder(this, Locale.getDefault());
+
+    addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+    String city = addresses.get(0).getLocality();
+    String state = addresses.get(0).getAdminArea();
+    String country = addresses.get(0).getCountryName();
+    String postalCode = addresses.get(0).getPostalCode();
+    String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+}
---
+++ app/src/main/java/com/asu/mc/digitalassist/activities/services/FetchAddressIntentService.java
@@ -0,0 +1,8 @@
+package com.asu.mc.digitalassist.activities.services;
+
+/**
+ * Created by anurag on 4/17/17.
+ */
+public class FetchAddressIntentService {
+
+}
